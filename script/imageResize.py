b50886c56ac179745eb67f7a2f81c52bfd1beb46
SimonCK666 <SimonYang223@163.com>
2022-08-31 18:00:07 +0800

    edit_prof_img

--- script/imageResize.py
+++ script/imageResize.py
@@ -2,7 +2,7 @@
 Author: SimonCK666 SimonYang223@163.com
 Date: 2022-08-31 16:41:02
 LastEditors: SimonCK666 SimonYang223@163.com
-LastEditTime: 2022-08-31 17:07:03
+LastEditTime: 2022-08-31 17:58:15
 FilePath: /SimonCK666.github.io/script/imageResize.py
 Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
 '''
@@ -16,7 +16,7 @@ def ResizeImage(filein, fileout, width, height):
 
 if __name__ == "__main__":
     filein = r'../assets//resized//prof_pic_yh.jpeg'
-    fileout = r'../assets//resized//prof_pic-480x659.jpeg'
+    fileout = r'../assets//resized//prof_pic-480x659.png'
     width = 480
     height = 659
     ResizeImage(filein, fileout, width, height)
